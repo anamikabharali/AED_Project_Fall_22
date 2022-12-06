0e2ea96bb59b912bfb7906cb2be3867be630d2e6
anamikabharali <anamikabharali683@gmail.com>
2022-12-06 01:00:56 -0500

    Changed initial classes

---
+++ AVAProject/src/bbank/ConfigureBusiness.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bbank;
+import bbank.Role.DonarRole;
+import bbank.UserAccount.UserAccount;
+
+import bbank.WorkQueue.WorkRequest;
+import bbank.Donor.DeliveryManDirectory;
+import bbank.user;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+/**
+ *
+ */
+public class ConfigureBusiness {
+    private bbank.Donor.DeliveryManDirectory ddirectory;
+    public static Bbank configure() {
+         return null;
+}
+}
