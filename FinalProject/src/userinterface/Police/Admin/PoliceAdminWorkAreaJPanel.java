cd4f16cce5081f3d07632e41970529f039f0bb25
anamikabharali <anamikabharali683@gmail.com>
2022-12-07 11:48:16 -0500

    Police workarea ui

--- FinalProject/src/userinterface/Police/Admin/PoliceAdminWorkAreaJPanel.java
+++ FinalProject/src/userinterface/Police/Admin/PoliceAdminWorkAreaJPanel.java
@@ -26,15 +26,19 @@ public class PoliceAdminWorkAreaJPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setBackground(new java.awt.Color(76, 89, 115));
+        setMaximumSize(new java.awt.Dimension(788, 600));
+        setMinimumSize(new java.awt.Dimension(788, 600));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 788, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 600, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
