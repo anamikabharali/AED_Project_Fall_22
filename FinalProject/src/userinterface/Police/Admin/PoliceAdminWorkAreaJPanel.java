32927390403e15e9ce475da5591a835276e288fa
anamikabharali <anamikabharali683@gmail.com>
2022-12-07 12:55:19 -0500

    #5 Update in userinterface.Police.AdminWorkArea

--- FinalProject/src/userinterface/Police/Admin/PoliceAdminWorkAreaJPanel.java
+++ FinalProject/src/userinterface/Police/Admin/PoliceAdminWorkAreaJPanel.java
@@ -55,14 +55,17 @@ public class PoliceAdminWorkAreaJPanel extends javax.swing.JPanel {
         manageempbtn = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         enterprisetxt = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(76, 89, 115));
         setMaximumSize(new java.awt.Dimension(788, 600));
         setMinimumSize(new java.awt.Dimension(788, 600));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Optima", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("POLICE ADMINISTRATION");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 100, -1, -1));
 
         manageordbtn.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         manageordbtn.setText("MANAGE ORGANISATION");
@@ -72,6 +75,7 @@ public class PoliceAdminWorkAreaJPanel extends javax.swing.JPanel {
                 manageordbtnActionPerformed(evt);
             }
         });
+        add(manageordbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 450, 185, 38));
 
         manageuserbtn.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         manageuserbtn.setText("MANAGE USER");
@@ -80,6 +84,7 @@ public class PoliceAdminWorkAreaJPanel extends javax.swing.JPanel {
                 manageuserbtnActionPerformed(evt);
             }
         });
+        add(manageuserbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 450, 142, 38));
 
         manageempbtn.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         manageempbtn.setText("MANAGE EMPLOYEE");
@@ -89,55 +94,21 @@ public class PoliceAdminWorkAreaJPanel extends javax.swing.JPanel {
                 manageempbtnActionPerformed(evt);
             }
         });
+        add(manageempbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 450, -1, 38));
 
         jLabel2.setFont(new java.awt.Font("Optima", 0, 16)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Enterprise:");
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 330, -1, -1));
 
         enterprisetxt.setBackground(new java.awt.Color(255, 255, 255));
         enterprisetxt.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         enterprisetxt.setToolTipText("");
         enterprisetxt.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+        add(enterprisetxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 330, 165, 20));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(124, 124, 124)
-                        .addComponent(manageordbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(79, 79, 79)
-                        .addComponent(manageempbtn)
-                        .addGap(83, 83, 83)
-                        .addComponent(manageuserbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(262, 262, 262)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(36, 36, 36)
-                                .addComponent(enterprisetxt, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jLabel1))))
-                .addContainerGap(129, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(71, 71, 71)
-                .addComponent(jLabel1)
-                .addGap(89, 89, 89)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(enterprisetxt, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(96, 96, 96)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(manageordbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(manageuserbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(manageempbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(257, Short.MAX_VALUE))
-        );
+        jLabel3.setIcon(new javax.swing.ImageIcon("/Users/anamikabharali/Downloads/imageedit_3_2921333366.jpg")); // NOI18N
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 880, 230));
     }// </editor-fold>//GEN-END:initComponents
 
     private void manageordbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manageordbtnActionPerformed
@@ -169,6 +140,7 @@ public class PoliceAdminWorkAreaJPanel extends javax.swing.JPanel {
     private javax.swing.JLabel enterprisetxt;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JButton manageempbtn;
     private javax.swing.JButton manageordbtn;
     private javax.swing.JButton manageuserbtn;
