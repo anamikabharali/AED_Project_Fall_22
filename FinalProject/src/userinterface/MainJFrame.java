9b449190a9aaa2da8dfa89e831514a9b8f450e2c
anamikabharali <anamikabharali683@gmail.com>
2022-12-09 12:51:53 -0500

    UODATE

--- FinalProject/src/userinterface/MainJFrame.java
+++ FinalProject/src/userinterface/MainJFrame.java
@@ -55,24 +55,30 @@ public class MainJFrame extends javax.swing.JFrame {
         logoutbtn = new javax.swing.JButton();
         signupbtn = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(113, 128, 152));
 
         jPanel2.setBackground(new java.awt.Color(76, 89, 115));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         logolbl.setIcon(new javax.swing.ImageIcon("/Users/anamikabharali/Desktop/Screen Shot 2022-12-07 at 10.26.10 AM.png")); // NOI18N
         logolbl.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        jPanel2.add(logolbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 166, -1));
 
         jLabel2.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("USERNAME");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(44, 243, -1, -1));
 
         jLabel3.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("PASSWORD");
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(44, 330, -1, -1));
+        jPanel2.add(usernametxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 279, 154, -1));
+        jPanel2.add(passwordtxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 366, 154, -1));
 
         loginbtn.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         loginbtn.setText("LOG IN");
@@ -82,6 +88,7 @@ public class MainJFrame extends javax.swing.JFrame {
                 loginbtnActionPerformed(evt);
             }
         });
+        jPanel2.add(loginbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 433, -1, -1));
 
         logoutbtn.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         logoutbtn.setText("LOG OUT");
@@ -91,6 +98,7 @@ public class MainJFrame extends javax.swing.JFrame {
                 logoutbtnActionPerformed(evt);
             }
         });
+        jPanel2.add(logoutbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(42, 473, -1, -1));
 
         signupbtn.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         signupbtn.setText("SIGN UP !");
@@ -100,66 +108,14 @@ public class MainJFrame extends javax.swing.JFrame {
                 signupbtnActionPerformed(evt);
             }
         });
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(logolbl, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(49, 49, 49))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(loginbtn)
-                        .addGap(63, 63, 63))))
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(passwordtxt, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
-                            .addComponent(usernametxt)))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(44, 44, 44)
-                        .addComponent(jLabel3))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(42, 42, 42)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(signupbtn)
-                            .addComponent(logoutbtn))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addComponent(logolbl)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(18, 18, 18)
-                .addComponent(usernametxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(28, 28, 28)
-                .addComponent(jLabel3)
-                .addGap(18, 18, 18)
-                .addComponent(passwordtxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(44, 44, 44)
-                .addComponent(loginbtn)
-                .addGap(18, 18, 18)
-                .addComponent(logoutbtn)
-                .addGap(18, 18, 18)
-                .addComponent(signupbtn)
-                .addGap(65, 65, 65))
-        );
+        jPanel2.add(signupbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(42, 513, -1, -1));
 
         jSplitPane1.setLeftComponent(jPanel2);
 
-        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel3.setLayout(new java.awt.CardLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("/Users/anamikabharali/Desktop/Screen Shot 2022-12-07 at 4.28.25 PM.png")); // NOI18N
-        jLabel1.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        jPanel3.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 730, 550));
+        jLabel4.setIcon(new javax.swing.ImageIcon("/Users/anamikabharali/Desktop/Screen Shot 2022-12-09 at 12.41.19 PM.png")); // NOI18N
+        jPanel3.add(jLabel4, "card2");
 
         jSplitPane1.setRightComponent(jPanel3);
 
@@ -324,9 +280,9 @@ public class MainJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
