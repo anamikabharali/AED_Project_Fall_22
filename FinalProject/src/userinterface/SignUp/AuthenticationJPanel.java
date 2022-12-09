b9ee2b484073939e63441c73e1659a3f15d3ddd8
anamikabharali <anamikabharali683@gmail.com>
2022-12-08 20:57:27 -0500

    ManageEnterpriseJPanel STARTING UI

--- FinalProject/src/userinterface/SignUp/AuthenticationJPanel.java
+++ FinalProject/src/userinterface/SignUp/AuthenticationJPanel.java
@@ -1,9 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 package userinterface.SignUp;
+
+/**
+ *
+ * @author anamikabharali
+ */
 
 import Business.EcoSystem;
 import java.awt.CardLayout;
@@ -16,10 +20,6 @@ import javax.swing.JPanel;
 import userinterface.Police.felony.FelonyWorkAreaJPanel;
 import utility.Validate;
 
-/**
- *
- * @author srivaishnaviaekkati
- */
 public class AuthenticationJPanel extends javax.swing.JPanel {
 
     /**
@@ -54,83 +54,53 @@ JPanel container;
         jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         authenticatetxtfield = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
-        setBackground(new java.awt.Color(0, 153, 153));
-        setMaximumSize(new java.awt.Dimension(750, 750));
-        setMinimumSize(new java.awt.Dimension(750, 750));
-        setPreferredSize(new java.awt.Dimension(750, 750));
+        setBackground(new java.awt.Color(76, 89, 115));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Optima", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Enter your email id for the verification ");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 250, -1, -1));
+        add(emailtxtfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 300, 238, -1));
+        add(authtxtfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 460, 110, -1));
 
-        jButton1.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jButton1.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         jButton1.setText("Send Mail");
+        jButton1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 340, 120, -1));
 
-        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Optima", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Please enter the four digit code that has been sent to your email entered above");
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 430, 500, -1));
 
-        authenticatetxtfield.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        authenticatetxtfield.setFont(new java.awt.Font("Optima", 0, 14)); // NOI18N
         authenticatetxtfield.setText("Authenticate");
+        authenticatetxtfield.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         authenticatetxtfield.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 authenticatetxtfieldActionPerformed(evt);
             }
         });
+        add(authenticatetxtfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 500, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(225, 225, 225)
-                        .addComponent(emailtxtfield, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(283, 283, 283)
-                        .addComponent(authtxtfield, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(275, 275, 275)
-                        .addComponent(authenticatetxtfield))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(291, 291, 291)
-                        .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(109, 109, 109)
-                        .addComponent(jLabel2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(179, 179, 179)
-                        .addComponent(jLabel1)))
-                .addContainerGap(156, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(129, 129, 129)
-                .addComponent(jLabel1)
-                .addGap(101, 101, 101)
-                .addComponent(emailtxtfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(39, 39, 39)
-                .addComponent(jButton1)
-                .addGap(82, 82, 82)
-                .addComponent(jLabel2)
-                .addGap(61, 61, 61)
-                .addComponent(authtxtfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(78, 78, 78)
-                .addComponent(authenticatetxtfield)
-                .addContainerGap(119, Short.MAX_VALUE))
-        );
+        jLabel3.setFont(new java.awt.Font("Optima", 1, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("AUTHENTICATION FORM");
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 120, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        
-         try {
+
+        try {
               Validate.sendMessage1(emailtxtfield.getText(),n);
            
             JOptionPane.showMessageDialog(this,"Mail has been sent");
@@ -141,6 +111,7 @@ JPanel container;
             Logger.getLogger(FelonyWorkAreaJPanel.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(this,"Mail has not  been sent");
         }
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void authenticatetxtfieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_authenticatetxtfieldActionPerformed
@@ -170,5 +141,6 @@ JPanel container;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
